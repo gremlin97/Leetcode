38b73e7a64bfaadc1611e1c1921f6ed34348bd82
Kunal Kasodekar <kasodekarsunil@gmail.com>
2023-08-20 10:03:37 -0700

    Time: 45 ms (41.28%), Space: 16.4 MB (40.35%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -10,11 +10,9 @@ class Solution:
         hashmap = {}
         
         for p, w in zip(pattern, s):
-            if w in hashmap.values() and p not in hashmap.keys():
+            if (w in hashmap.values() and p not in hashmap.keys()) or (w not in hashmap.values() and p in hashmap.keys()):
                 return False
             elif w in hashmap.values() and hashmap[p]!= w:
-                return False
-            elif w not in hashmap.values() and p in hashmap.keys():
                 return False
             else:
                 hashmap[p] = w
