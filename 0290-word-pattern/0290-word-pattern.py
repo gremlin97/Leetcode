856f7b396a0efb2c019b578a75947836bb711f57
Kunal Kasodekar <kasodekarsunil@gmail.com>
2023-08-20 10:00:35 -0700

    Time: 39 ms (72.82%), Space: 16.2 MB (73.85%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -2,41 +2,25 @@ from collections import Counter
 
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        s = s.split()
+        s = s.split(" ")
         
-        if len(pattern) != len(s):
+        if len(s)!=len(pattern):
             return False
         
         hashmap = {}
         
-        for c, w in zip(pattern, s):
-            if c in hashmap:
-                if hashmap[c]!=w:
-                    return False
-            elif w in hashmap.values():
+        for p, w in zip(pattern, s):
+            print(p,w)
+            if w in hashmap.values() and p not in hashmap.keys():
+                return False
+            elif w in hashmap.values() and hashmap[p]!= w:
+                return False
+            elif w not in hashmap.values() and p in hashmap.keys():
                 return False
             else:
-                hashmap[c]=w
+                hashmap[p] = w
+        
         return True
-#         for x in s:
-#             if x in hashmap:
-#                 hashmap[x] += 1
-#             else:
-#                 hashmap[x] = 1
-        
-#         print(hashmap)
-        
-#         a = hashmap.values()
-        
-#         b = dict(Counter(pattern))
-#         b = b.values()
-        
-#         print(a,b)
-        
-#         if list(a) == list(b):
-#             return True
-#         else:
-#             return False
             
         
             
