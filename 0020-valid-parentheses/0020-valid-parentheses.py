aadb9830a1e44ec995552c9244a4de41992d94f2
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-01-14 00:10:24 -0700

    Time: 44 ms (25.26%), Space: 17.3 MB (37.80%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -8,16 +8,12 @@ class Solution:
             if x not in hashmap:
                 stack.append(x)
             else:
-                if stack:
-                    print('Yes')
-                    print(stack[-1], hashmap[x])
-                    if stack[-1] == hashmap[x]:
+                if stack and stack[-1] == hashmap[x]:
                         stack.pop()
-                    else:
-                        return False
                 else:
                     return False
-        if len(stack)==0:
-            return True
-        return False
+        
+        if stack:
+            return False
+        return True
         
