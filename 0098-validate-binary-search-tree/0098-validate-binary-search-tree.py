12cd922e194d1d89630ab47d94b050eefa07a001
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-02-02 16:42:36 -0700

    Time: 46 ms (47.17%), Space: 19 MB (56.51%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -19,7 +19,7 @@ class Solution:
         inorder(root)
         
         print(trav, set(trav))
-        if len(trav)!= len(set(trav)):
+        if trav!= sorted(list(set(trav))):
             return False
         return trav == sorted(trav)
         
