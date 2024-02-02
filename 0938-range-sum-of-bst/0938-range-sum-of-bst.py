58d22747d72b8103aa1874ae0bbb855b1ff06080
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-02-02 14:22:41 -0700

    Time: 132 ms (37.63%), Space: 23.8 MB (92.20%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.py
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.py
@@ -29,7 +29,7 @@ class Solution:
                 s += trav[i]
                 
             if high == trav[i]:
-                f = False
+                break
                 
         
         return s
