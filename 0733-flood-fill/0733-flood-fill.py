c602ad2052419ce819f7aa692e6f1ec0f6f67302
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-02-13 22:49:36 -0700

    Time: 66 ms (70.68%), Space: 16.9 MB (54.37%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.py
@@ -0,0 +1,20 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        org = image[sr][sc]
+        
+        def floodFillUtil(r, c, fillcolor):
+            if r>(len(image)-1) or c>(len(image[0])-1) or r<0 or c<0 or image[r][c] == color or image[r][c] != fillcolor:
+                return
+            print(r,c)
+            image[r][c] = color
+            directions = [[1,0],[-1,0],[0,-1],[0,1]]
+            
+            for a, b in directions:
+                floodFillUtil(r+a, c+b, org)
+                
+        floodFillUtil(sr,sc, org)
+        
+        return image
+                
+        
+        
