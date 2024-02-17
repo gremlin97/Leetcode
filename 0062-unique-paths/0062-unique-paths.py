ffcc9514fb9aaa9a5a17c077bf8e7895e0d52739
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-02-17 01:39:26 -0700

    Time: 33 ms (79.57%), Space: 16.6 MB (59.60%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,15 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        memo = [[-1] * n for _ in range(m)]
+        
+        def paths(x, y):
+            if x == m - 1 and y == n - 1:
+                return 1
+            if x == m or y == n:
+                return 0
+            if memo[x][y] > 0:
+                return memo[x][y]
+            memo[x][y] = (paths(x+1, y) + paths(x, y+1))
+            return memo[x][y]
+        
+        return paths(0, 0)
