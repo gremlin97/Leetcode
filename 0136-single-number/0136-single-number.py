002ff44396b5caf1f69304eb7954cd615076719a
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-02-24 23:43:38 -0700

    Time: 111 ms (64.13%), Space: 19.5 MB (42.76%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,8 +1,4 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        d = Counter(nums)
-        for k, v in d.items():
-            if v == 1:
-                return k
-            
+        return (2 * sum(set(nums)) - sum(nums))
         
