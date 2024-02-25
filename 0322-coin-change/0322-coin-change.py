649050b5305ed957aba586aa05db0d0e037f3046
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-02-25 02:39:11 -0700

    Time: 683 ms (92.13%), Space: 27.4 MB (20.68%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,7 +1,7 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int: 
         memo = [-1] * (amount + 1)
-        # amount = sorted(coins, reverse=True)
+        coins = sorted(coins, reverse=True)
         
         @lru_cache
         def dfs(t):
