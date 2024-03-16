030fd2d6000eb82e5c50faca2504aa3d03d4e369
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-03-15 20:18:54 -0700

    Time: 34 ms (93.53%), Space: 16.8 MB (36.55%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,25 +1,3 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        d = {}
-        for i in range(len(s)):
-            if s[i] not in d:
-                d[s[i]] = []
-                d[s[i]].append(i)
-            else:
-                d[s[i]].append(i)
-                
-        e = {}
-        for i in range(len(t)):
-            if t[i] not in e:
-                e[t[i]] = []
-                e[t[i]].append(i)
-            else:
-                e[t[i]].append(i)
-        
-        if list(e.values()) == list(d.values()):
-            return True
-        return False
-            
-                
-                
-        
+        return (len(set(zip(s,t))) == len(set(s))) and (len(set(zip(s,t))) == len(set(t)))
