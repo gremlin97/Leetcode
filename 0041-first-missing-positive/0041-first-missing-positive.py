5ac496530f536769fdd598bf82b80a540123b406
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-03-25 22:29:59 -0700

    Time: 282 ms (75.53%), Space: 34.8 MB (31.52%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -4,13 +4,9 @@ class Solution:
         for x in nums:
             if x>0:
                 s.add(x)
-        
-        print(s)
             
         for i in range(1, len(s) + 2):
-            # print(i)
             if i not in s:
-                # print('Here')
                 return i 
 
         
