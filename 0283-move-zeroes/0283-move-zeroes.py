5a076895f88250958d139fa1c78588077ea85d33
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-03-26 23:34:19 -0700

    Time: 132 ms (38.25%), Space: 18.1 MB (31.51%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -7,9 +7,7 @@ class Solution:
         slow = 0
         for fast in range(len(nums)):
             if nums[fast] != 0 and nums[slow] == 0:
-                t = nums[fast]
-                nums[fast] = nums[slow]
-                nums[slow] = t
+                nums[fast], nums[slow] = nums[slow], nums[fast]
             if nums[slow] != 0:
                 slow += 1
         
