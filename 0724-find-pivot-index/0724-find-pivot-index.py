530d31cd4a1b2e72930d1156cb6ec60b2dcc8a0e
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-03-28 15:24:46 -0700

    Time: 2257 ms (14.96%), Space: 18 MB (11.81%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -4,7 +4,6 @@ class Solution:
         for i in range(0, len(nums)):
             left_sum = sum(nums[0:i])
             right_sum = (total_sum - left_sum - nums[i])
-            print(left_sum, right_sum)
             if left_sum == (total_sum - left_sum - nums[i]):
                 return i
         return -1
