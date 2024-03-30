b85c8cbd18ee33ef4f8ed2ae2a8a23976c08d68e
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-03-29 17:42:20 -0700

    Time: 50 ms (9.18%), Space: 16.7 MB (30.55%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,13 +1,5 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        hashmap = {}
-        for x in arr:
-            if x not in hashmap:
-                hashmap[x] = 1
-            else:
-                hashmap[x]+=1
-            
-        if len(hashmap.values()) == len(set(list(hashmap.values()))):
-            return True
-        else:
-            return False
+        d = Counter(arr)
+        # print(sorted(list(d.values())), sorted(list(set(d.values()))))
+        return (sorted(list(d.values())) == sorted(list(set(d.values()))))
