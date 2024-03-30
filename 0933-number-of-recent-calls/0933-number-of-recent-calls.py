0f419d002597d34e53ba0d7db7adffb8a5bc4b48
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-03-30 11:49:32 -0700

    Time: 9833 ms (5.00%), Space: 22.3 MB (16.64%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -0,0 +1,25 @@
+class RecentCounter:
+
+    def __init__(self):
+        self.range = []
+        self.requests = []
+        self.res = 0  
+
+    def ping(self, t: int) -> int:
+        self.requests.append(t)
+        self.range = [t-3000, t]
+        # print(self.requests)
+        
+        i = 0
+        while(i<len(self.requests)):
+            if self.requests[i] not in range(self.range[0], self.range[1] + 1):
+                self.requests.pop(0)
+                i -= 1
+            i+=1
+        
+        # print(len(self.requests))
+        return len(self.requests)
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
