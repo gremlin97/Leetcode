481b81e080bf6835ada6d7c71f8b7c7ef4103b64
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-03-30 20:38:14 -0700

    Time: 77 ms (8.71%), Space: 17.1 MB (61.19%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        def dfs(r, v):
+            if r[v] == []:
+                r[v] = ['v']
+            for i in range(len(r[v])):
+                key = r[v][i]
+                print(key)
+                if key != 'v':
+                    r[v][i] = 'v'
+                    dfs(r, key)
+        
+        dfs(rooms, 0)
+        rooms = sum(rooms,[])
+        for x in rooms:
+            if x != 'v':
+                return False
+        return True
+                
+            
+            
+        
