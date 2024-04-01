8bf760e5775e1b576d520bc0df6fb7be509bf27f
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-03-31 23:35:49 -0700

    Time: 131 ms (45.28%), Space: 31 MB (52.16%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,41 +1,43 @@
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.isEnd = False
-
+        self.end = False
+    
 class Trie:
 
     def __init__(self):
-        self.root = TrieNode()
-        
+        self.node = TrieNode()
+    
     def insert(self, word: str) -> None:
-        curr = self.root
+        n = self.node
         for c in word:
-            if c not in curr.children:
-                curr.children[c] = TrieNode()
-            curr = curr.children[c]
-        curr.isEnd = True
+            if c not in n.children:
+                n.children[c] = TrieNode()
+            n = n.children[c]
+        n.end = True
                 
-
-    def search(self, word: str) -> bool:
-        curr = self.root
-        for c in word:
-            if c not in curr.children:
-                return False
-            curr = curr.children[c]
-        if curr.isEnd != True:
-            return False
-        return True
         
+    def search(self, word: str) -> bool:
+        n = self.node
+        for c in word:
+            if c not in n.children:
+                return False
+            else:
+                n = n.children[c]
+        return n.end
+
 
     def startsWith(self, prefix: str) -> bool:
-        curr = self.root
+        n = self.node
         for c in prefix:
-            if c not in curr.children:
+            if c not in n.children:
                 return False
-            curr = curr.children[c]
+            else:
+                n = n.children[c]
         return True
         
+
+
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
 # obj.insert(word)
