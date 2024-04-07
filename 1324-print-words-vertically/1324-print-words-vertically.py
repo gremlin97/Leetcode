b9ffc3e032eb312c76918b31d6c2cb9b34b5ba33
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-04-07 00:04:59 -0700

    Time: 20 ms (98.91%), Space: 16.5 MB (29.93%) - LeetHub

--- 1324-print-words-vertically/1324-print-words-vertically.py
+++ 1324-print-words-vertically/1324-print-words-vertically.py
@@ -5,12 +5,10 @@ class Solution:
         for x in s:
             max_substring_len = max(max_substring_len, len(x))
         res = []        
-        # print(max_substring_len)
             
         for i in range(max_substring_len):
             temp = []
             for j in range(len(s)):
-                # print(s[j],i, temp)
                 if i > (len(s[j]) - 1):
                     temp.append(' ')
                 else:
