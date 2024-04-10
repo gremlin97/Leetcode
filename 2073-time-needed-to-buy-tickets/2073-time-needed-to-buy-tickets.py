6059f5c64bf038037da4f827e60f6153972e5895
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-04-09 17:02:43 -0700

    Time: 45 ms (60.32%), Space: 16.7 MB (15.10%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -1,10 +1,12 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        # val = tickets[k]
-        # res = val
-        # tickets.pop(k)
-        # for x in tickets:
-        #     print(tickets, x, abs(x-val))
-        #     res += min(val, abs(x-val))
-        # return res
-        return sum(min(x, tickets[k] if i <= k else tickets[k] - 1) for i, x in enumerate(tickets))
+        val = tickets[k]
+        res = 0
+        for i, x in enumerate(tickets):
+            if i <= k:
+                res += min(val, x)
+            else:
+                res += min(val-1, x)
+        return res
+
+        
