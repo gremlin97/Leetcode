97de547d7b85c9d947993cec86ca2fa3150b4a4d
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-04-17 17:30:00 -0700

    Time: 438 ms (39.83%), Space: 17 MB (65.66%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,6 +1,5 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        visited = set()
         res = 0
         
         def neigh(r,c):
@@ -12,28 +11,11 @@ class Solution:
                         n+=1
             return n
         
-#         def dfs(r,c):
-#             if 0<=r<len(grid) and 0<=c<len(grid[0]) and grid[r][c] == 1 and(r,c) not in visited:
-#                 visited.add((r,c))
-#                 grid[r][c] = -1
-#                 directions = [[1,0],[-1,0],[0,1],[0,-1]]
-#                 for x,y in directions:
-#                     dfs(r+x,c+y)
-#             else:
-#                 return
-        
-#         for a in range(len(grid)):
-#             for b in range(len(grid[0])):
-#                 dfs(a,b)
-        
         for a in range(len(grid)):
             for b in range(len(grid[0])):
                 if grid[a][b] == 1:
                     t = neigh(a,b)
-                    print(a,b,t)
                     res = res + (4-t)
-                
-        print(neigh(0,1))
         
         return res
                     
