6f80f5beb5542a771ac55349f36a9d67834f2f17
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-05-13 14:23:52 -0700

    Time: 74 ms (22.02%), Space: 18.1 MB (10.94%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.py
+++ 0506-relative-ranks/0506-relative-ranks.py
@@ -1,14 +1,39 @@
 class Solution:
     def findRelativeRanks(self, score: List[int]) -> List[str]:
+        # res = []
+        # s = sorted(score, reverse = True)
+        # r = ['Gold Medal', 'Silver Medal', 'Bronze Medal'] + list(range(4,len(s)+1))
+        # z = dict(zip(s,r))
+        # for x in score:
+        #     if type(z[x]) != string:
+        #         res.append(str(z[x]))
+        #     else:
+        #         res.append(str(z[x]))
+        # return res
+        
+        
+        
         res = []
-        s = sorted(score, reverse = True)
-        r = ['Gold Medal', 'Silver Medal', 'Bronze Medal'] + list(range(4,len(s)+1))
-        z = dict(zip(s,r))
-        for x in score:
-            if type(z[x]) != string:
-                res.append(str(z[x]))
+        heap = []
+        for i,v in enumerate(score):
+            heapq.heappush(heap,(-v, i))
+        print(heap)
+        res = [0] * len(score) 
+        r = 1
+        # print(heapq.heappop(heap)[1])
+        while heap:
+            if r == 1:
+                res[heapq.heappop(heap)[1]] = 'Gold Medal'
+            elif r == 2:
+                res[heapq.heappop(heap)[1]] = 'Silver Medal'
+            elif r == 3:
+                res[heapq.heappop(heap)[1]] = 'Bronze Medal'
             else:
-                res.append(str(z[x]))
+                res[heapq.heappop(heap)[1]] = str(r)
+            r += 1
+        
         return res
+                
+        
         
         
