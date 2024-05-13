9d329330dd02f405b79a07d4a72034fe68653728
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-05-13 13:09:49 -0700

    Time: 50 ms (6.87%), Space: 16.5 MB (92.70%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.py
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.py
@@ -2,13 +2,11 @@ class Solution:
     def count(self, grid, c):
         c0, c1 = 0, 0
         for r in range(len(grid)):
-            # print(grid[r][c],r,c)
             if grid[r][c] == 1:
                 c1 += 1
             else:
                 c0 += 1
-            
-        # print(c0, c1)
+
         if c0 > c1:
             return True
         return False
@@ -18,23 +16,17 @@ class Solution:
             if grid[r][0] != 1:
                 for c in range(len(grid[0])):
                     grid[r][c] = grid[r][c] ^ 1
-        
-        # print(grid)
-        # print('Count', self.count(grid,2))
-        
+
         for c in range(len(grid[0])):
             if self.count(grid, c):
                 for r in range(len(grid)):
                     grid[r][c] = grid[r][c] ^ 1
                     
-        # print(grid)
         s = 0
         for r in range(len(grid)):
             temp = ''
             for c in range(len(grid[0])):
-                # print(grid[r][c])
                 temp += str(grid[r][c])
-                # print(temp)
             s += int(temp,2)
         
         return s
