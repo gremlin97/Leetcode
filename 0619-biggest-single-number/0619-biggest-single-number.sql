ab3f75542a5c1bfb57cf30ef3b8cbe305b2487ef
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-06-16 15:25:00 -0700

    Time: 328 ms (99.11%), Space: 0B (100.00%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -2,11 +2,11 @@
 with cte as (
     select num, count(num) as cnt from MyNumbers
     group by num
+    having count(num) = 1
 )
 
-select (
-select num from cte
-where cnt = 1
+
+select max(num) as  num from cte
 order by num desc
-limit 1 ) as num
+limit 1 
 
