f11cf836e67ba95ebc222b58bbdf4036bf3fcae7
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-06-16 18:17:23 -0700

    Time: 309 ms (94.49%), Space: 0B (100.00%) - LeetHub

--- 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -3,9 +3,12 @@ with cte1 as (
     from Employees
     where salary < 30000
     order by employee_id
+),
+cte2 as (
+    select employee_id from Employees
 )
 
 select employee_id from cte1
-where manager_id not in (select employee_id from Employees)
+where manager_id not in (select * from cte2)
 
 
