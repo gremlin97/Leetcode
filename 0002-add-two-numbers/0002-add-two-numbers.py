a1dd13d63076d6de27b2902c22f75c62779b0683
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-07-04 14:06:02 -0700

    Time: 55 ms (56.46%), Space: 16.5 MB (68.80%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,37 +5,41 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        def getdigits(head: str):
-            res = ''
-            curr = head
+        # sum l1
+        s1 = ''
+        curr = l1
+        while(curr):
+            s1 += str(curr.val)
+            curr = curr.next
+        
+        # sum l2
+        s2 = ''
+        curr = l2
+        while(curr):
+            s2 += str(curr.val)
+            curr = curr.next
+        
+        s1 = int(s1[::-1])
+        s2 = int(s2[::-1])
+        
+        s3 = str(s1 + s2)
+        s3 = list(s3)
+        s3.reverse()
+        
+        l3 = ListNode()
+        l3.val = s3[0]
+        
+        curr = l3
+        for i in range(1,len(s3)):
+            temp = ListNode()
+            temp.val = s3[i]
+            temp.next = None
             
-            while(curr != None):
-                res += str(curr.val)
-                curr = curr.next
+            curr.next = temp
+            curr = curr.next
+        
+        return l3
+        
+        
             
-            return res
-        
-        d1 = getdigits(l1)
-        d1 = d1[::-1]
-        d2 = getdigits(l2)
-        d2= d2[::-1]
-        
-        print(d1, d2)
-        s = list(str(int(d1) + int(d2)))
-        s.reverse()
-        
-        head = ListNode(val = s[0]) 
-        curr = head
-        
-        for i in range(1, len(s)):
-            new = ListNode(val = s[i])
-            curr.next = new
-            curr = new
-        
-        return head
-            
-            
-            
-                
-        
         
