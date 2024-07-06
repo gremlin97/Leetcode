9515f392b21acfb1ed4dde24b41d1dc6c417ab38
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-07-06 15:06:05 -0700

    Time: 258 ms (40.50%), Space: 18.9 MB (53.38%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,28 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        res = 0
         
         def dfs(r, c):
-            if r>len(grid)-1 or c>len(grid[0])-1 or r<0 or c<0 or grid[r][c]!="1":
-                return
             
-            grid[r][c] = "-1"
-            directions = [[1,0], [-1,0], [0,1], [0,-1]]
-            
-            for a,b in directions:
-                dfs(r+a, c+b)
-                
-        count = 0
-        for r in range(0, len(grid)):
-            for c in range(0, len(grid[0])):
-                if grid[r][c] == "1":
-                    dfs(r,c)
-                    count += 1
-        
-        return count
-                    
-                        
+            if 0<=r<len(grid) and 0<=c<len(grid[0]):
+                if grid[r][c] == '1': 
 
-    
-            
+                    grid[r][c] = '-1'
+
+                    trav = [[0,1],[0,-1],[1,0],[-1,0]]
+
+                    for x,y in trav:
+                        dfs(r+x, c+y)
+
+                        
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == '1':
+                    dfs(r,c)
+                    res += 1
+        
+        return res
                     
+            
+            
         
