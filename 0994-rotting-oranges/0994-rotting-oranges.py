4e75d7c25fbb1ee1ffb56295f8bdff0fc593437a
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-07-06 17:57:46 -0700

    Time: 55 ms (28.56%), Space: 16.8 MB (5.22%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,32 +1,38 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         q = []
-        directions = [[0,1],[0,-1],[1,0],[-1,0]]
-        m = 0
+        org = 0
+        rows, cols = len(grid), len(grid[0])
+        res = 0
         
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 2:
-                    q.append([i,j])
-                
-        while(q):
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    q.append([r,c])
+                if grid[r][c] == 1:
+                    org += 1
+        
+        
+        while q and org>0:
+            dr = [[0,1],[0,-1],[1,0],[-1,0]]
             for _ in range(len(q)):
-                node = q.pop(0)
-                for d in directions:
-                    new_x = node[0] + d[0]
-                    new_y = node[1] + d[1]
-                    if 0 <= new_x < len(grid) and 0 <= new_y < len(grid[0]):
-                        if grid[new_x][new_y] == 1:
-                            grid[new_x][new_y] = 2
-                            q.append([new_x,new_y])
-            m += 1
+                n = q.pop(0)
+                
+                for a, b in dr:
+                    nr, nc = n[0]+a, n[1]+b
+                    if 0<=nr<rows and 0<=nc<cols and grid[nr][nc] == 1:
+                        grid[nr][nc] = 2
+                        q.append([nr, nc])
+                        org -= 1
+                        print(grid)
+            res += 1
+        
+        print(res, org, grid)
+        
+        if org == 0:
+            return res
+        return -1
+                
             
-        if 1 not in sum(grid,[]):
-            return max(0,m-1)
-        else:
-            return -1
-            
-                        
-                    
                 
             
