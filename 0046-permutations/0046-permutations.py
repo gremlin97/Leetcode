c51b6eec43321a254c1417787ba36a787aba9491
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-07-22 01:29:17 -0700

    Time: 44 ms (32.54%), Space: 16.8 MB (21.92%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,18 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        
+        def recur(curr):
+            if len(curr) == len(nums):
+                res.append(curr.copy())
+                return 
+            
+            for x in nums:
+                if x not in curr:
+                    curr.append(x)
+                    recur(curr)
+                    curr.pop()
+        
+        recur([])
+        return res
+        
