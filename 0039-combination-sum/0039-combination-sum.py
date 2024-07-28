3dfe8a58d9978732ba61fa4cce8e33c92f5fc845
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-07-27 21:05:06 -0700

    Time: 54 ms (51.34%), Space: 16.6 MB (31.60%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,18 +1,16 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
         subsets = []
+        res = []
         
         def recur(i, t):
-            #print(subsets, res, t, i)
-            if t == target:
-                res.append(subsets.copy())
-                return
-            
+            #print(subsets, res)
             if i == len(candidates):
+                if t == target:
+                    res.append(subsets.copy())
                 return
             
-            if t > target:
+            if t>target:
                 return
             
             subsets.append(candidates[i])
@@ -22,8 +20,10 @@ class Solution:
             subsets.pop()
             t -= candidates[i]
             recur(i+1, t)
-            
-        recur(0,0)
+        
+        recur(0, 0)
+        print(res)
         return res
+            
             
         
