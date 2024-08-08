a4dec237c05a6646eee511dc55fac83125db6587
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-08-08 08:08:49 -0700

    Time: 96 ms (24.61%), Space: 18.4 MB (46.44%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,34 +1,26 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         d = {i:[] for i in range(numCourses)}
-        
-        for course, pre in prerequisites:
-            d[course].append(pre)
-        
+        for cr,  pr in prerequisites:
+            d[cr].append(pr)
+            
         visited = set()
-        def dfs(i):
-            if i in visited:
+        def dfs(cr):
+            if cr in visited:
                 return False
-            if d[i] == []:
+            
+            if d[cr] == []:
                 return True
             
-            visited.add(i)
-            
-            for course in d[i]:
-                if not dfs(course):
-                    return False
-            
-            d[i] = []
-            visited.remove(i)
-            
+            visited.add(cr)
+            for c in d[cr]:
+                if not dfs(c): return False
+                
+            d[cr] = []
+            visited.remove(cr)
             return True
         
         for i in range(numCourses):
             if not dfs(i): return False
         return True
-    
-            
-            
-            
-            
         
