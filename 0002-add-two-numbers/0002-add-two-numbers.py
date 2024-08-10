4ffc7685d9b5e1d837e0fad06150417a684c8553
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-08-10 12:25:44 -0700

    Time: 48 ms (88.34%), Space: 16.7 MB (24.04%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,42 +5,35 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        # sum l1
         s1 = ''
-        curr = l1
-        while(curr):
-            s1 += str(curr.val)
-            curr = curr.next
-        
-        # sum l2
         s2 = ''
-        curr = l2
-        while(curr):
-            s2 += str(curr.val)
-            curr = curr.next
+        curr1 = l1
+        curr2 = l2
         
-        s1 = int(s1[::-1])
-        s2 = int(s2[::-1])
-        
-        s3 = str(s1 + s2)
-        s3 = list(s3)
-        s3.reverse()
-        
-        l3 = ListNode()
-        l3.val = s3[0]
-        
-        # sum all
-        curr = l3
-        for i in range(1,len(s3)):
-            temp = ListNode()
-            temp.val = s3[i]
-            temp.next = None
+        while curr1:
+            s1 += str(curr1.val)
+            curr1 = curr1.next
             
-            curr.next = temp
+        while curr2:
+            s2 += str(curr2.val)
+            curr2 = curr2.next
+            
+        s1 = s1[::-1]
+        s2 = s2[::-1]
+            
+        s3 = str(int(s1) + int(s2))
+        print(s3)
+        
+        s3 = list(s3)
+        
+        head = ListNode(int(s3[len(s3)-1]))
+        curr = head
+        
+        for i in range(len(s3)-2,-1,-1):
+            curr.next = ListNode(int(s3[i]))
             curr = curr.next
         
-        return l3
-        
-        
+        return head
+            
             
         
