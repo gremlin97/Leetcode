9651f4924d5e6a3fdfb0acfb8c408e9ea2441252
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-08-17 19:07:36 -0700

    Time: 75 ms (40.11%), Space: 19.8 MB (44.42%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -1,7 +1,7 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         last = -1
-        intervals.sort(key = lambda x:x[1])
+        intervals.sort(key = lambda x:x[0])
         for s,e in intervals:
             if s>=last:
                 last = e
