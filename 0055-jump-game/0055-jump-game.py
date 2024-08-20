2bda29afc72d3901f0dc1ad64ce96c9e19ad7b06
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-08-19 18:44:12 -0700

    Time: 347 ms (73.62%), Space: 17.6 MB (89.73%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,14 +1,13 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        gas = 0
+        jump = 0
         for x in nums:
-            if gas < 0:
+            if jump < 0:
                 return False
             else:
-                if gas<x:
-                    gas = x
-                gas -= 1
-        
+                if jump < x:
+                    jump = x
+            jump -= 1
+            
         return True
-                
         
