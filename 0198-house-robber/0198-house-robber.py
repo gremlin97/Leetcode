bb514acc0a8c17287ee941523faf371e9d4adcb8
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-08-29 14:52:29 -0700

    Time: 42 ms (15.56%), Space: 16.6 MB (5.78%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,12 +1,11 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        @lru_cache(None)
+        memo = {-1:0, 0:nums[0]}
         def prof(i):
-            if i<0:
-                return 0
-            elif i == 0:
-                return nums[0]
+            if i in memo:
+                return memo[i]
             else:
-                return max(prof(i-2)+nums[i], prof(i-1))
+                memo[i] = max(prof(i-2)+nums[i], prof(i-1))
+                return memo[i]
         
         return prof(len(nums)-1)
