6efabbbb340ee1c9c7a2175dd1ec33373f11f72a
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-08-29 17:53:53 -0700

    Time: 34 ms (85.03%), Space: 16.5 MB (49.23%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,4 +1,33 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        return str(int(num1)*int(num2))
+        a, b = 0, 0
+        res = ''
+        
+        num1 = list(num1)
+        num1.reverse()
+        
+        num2 = list(num2)
+        num2.reverse()
+
+        for i in range(len(num1)):
+            a = a + (ord(num1[i])-48)*(10**i)
+        
+        for i in range(len(num2)):
+            b = b + (ord(num2[i])-48)*(10**i)
+        
+        prod = a*b
+        
+        if prod == 0:
+            return '0'
+        
+        while prod:
+            dig = prod%10
+            # print('Digit',dig)
+            res = res + chr(dig + 48)
+            prod = prod//10
+        
+        return res[::-1]
+        
+        
+            
         
