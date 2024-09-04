e65a30ded175b3a69300e3eaa221d49a481c3938
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-09-04 08:49:30 -0700

    Time: 97 ms (28.54%), Space: 16.5 MB (86.49%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,41 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        s = set()
+        ns = ['1','2','3','4','5','6','7','8','9','.']
+        
+        # row
+        for i in range(9):
+            s.clear()
+            for j in range(9):
+                if board[i][j] in s or board[i][j] not in ns:
+                    return False
+                if board[i][j] != '.':
+                    s.add(board[i][j])
+        
+        #col
+        for i in range(9):
+            s.clear()
+            for j in range(9):
+                if board[j][i] in s or board[j][i] not in ns:
+                    return False
+                if board[j][i] != '.':
+                    s.add(board[j][i])
+        
+        
+        for k in range(0,9,3):
+            for l in range(0,9,3):
+                s.clear()
+                for i in range(3):
+                    for j in range(3):
+                        # print(k,l)
+                        # print(i+k,j+l,s)
+                        if board[i+k][j+l] in s or board[i+k][j+l] not in ns:
+                            return False
+                        if board[i+k][j+l] != '.':
+                            s.add(board[i+k][j+l])
+        
+        return True
+                
+            
+            
+        
