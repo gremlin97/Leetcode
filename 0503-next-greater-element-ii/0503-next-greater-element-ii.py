bebe9fb1cf8dee2e1ce485510c092d9225b44d9d
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-09-05 01:04:01 -0700

    Time: 158 ms (35.72%), Space: 19 MB (6.12%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -5,8 +5,8 @@ class Solution:
         
         for i,v in enumerate(2*nums):
             while stack and stack[-1][1] < v:
-                index, val = stack.pop()
-                res[index%len(nums)] = v
+                stackI, stackV = stack.pop()
+                res[stackI] = v
             stack.append([i%len(nums), v])
         return res
                 
