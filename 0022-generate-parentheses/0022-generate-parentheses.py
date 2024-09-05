6057c1c5db157c1f40ceb7420f7922a898a145cc
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-09-04 17:10:20 -0700

    Time: 39 ms (48.83%), Space: 16.8 MB (51.83%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -3,21 +3,20 @@ class Solution:
         stack = []
         res = []
         
-        def recur(c, o, stack):
+        def recur(c, o):
             if c == o == n:
                 res.append(''.join(stack))
-                return res
+                return
             
             if o<n:
                 stack.append('(')
-                recur(c,o+1, stack)
+                recur(c,o+1)
                 stack.pop()
             
             if c<o:
                 stack.append(')')
-                recur(c+1,o, stack)
+                recur(c+1,o)
                 stack.pop()
                 
-        recur(0,0,stack)
+        recur(0,0)
         return res
-        
