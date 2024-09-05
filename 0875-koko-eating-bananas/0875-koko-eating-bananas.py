0a70ad69445a0c44206039084edf5b8a25788ed7
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-09-05 11:43:27 -0700

    Time: 264 ms (62.30%), Space: 18.2 MB (14.69%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,44 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # 3 6 7 11 - 8
+        
+        '''
+        0 3 4 8
+        0 0 0 4
+        0 0 0 0
+        
+        '''
+        
+        def doSim(i):
+            ch = 0
+            for x in piles:
+                ch = ch + math.ceil(x/i)
+            if ch > h:
+                return False
+            else:
+                return True
+            
+        i = 1
+        
+        # for i in range(1,max(piles)+1):
+        #     if doSim(i):
+        #         return i
+        min_res = float('inf')
+        l, r = 1, max(piles)
+        while l<=r:
+            mid = l + (r-l)//2
+            
+            if doSim(mid):
+                min_res = min(min_res, mid)
+                r = mid - 1
+            else:
+                l = mid + 1
+        
+        return min_res
+                
+                
+        
+        
+        
+        
+        
