cef5234df0c778eecee7891a7bbafb49174c372a
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-09-09 14:59:57 -0700

    Time: 97 ms (22.58%), Space: 18.4 MB (62.75%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        n = numCourses
+        state = [0]*n
+        order = []
+        
+        adj = defaultdict(list)
+        for c,p in prerequisites:
+            adj[c].append(p)
+        
+        print(adj)
+        
+        unvisited, visiting, visited = 0, 1, 2
+                
+        def dfs(i):
+            if state[i] == visited:
+                return True
+            if state[i] == visiting:
+                return False
+            state[i] = visiting
+            
+            for neigh in adj[i]:
+                if not dfs(neigh):
+                    return False
+            
+            state[i] = visited
+            order.append(i)
+            return True
+                
+        for i in range(n):
+            if not dfs(i):
+                return []
+        return order
+            
+            
+        
