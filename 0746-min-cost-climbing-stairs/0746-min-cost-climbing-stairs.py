4dfdb989311f818a6d18633c91abcd84787e170f
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-09-27 12:53:03 -0700

    Time: 65 ms (5.72%), Space: 18.3 MB (14.44%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,10 +1,18 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        memo = {0:cost[0], 1:cost[1]}
-        def minCost(n):
-            if n in memo:
-                return memo[n]
-            memo[n] = min(minCost(n-1), minCost(n-2)) + cost[n]
-            return memo[n]
+        # c(n) = min(c(n-1),c(n-2)) + cost(n)
         
-        return min(minCost(len(cost)-1), minCost(len(cost)-2)) 
+        # def recur(n):
+        #     if n<=1:
+        #         return cost[0]
+        #     return min(recur(n-1), recur(n-2)) + cost[n-1]
+        # return recur(len(cost))
+        n = len(cost)
+        @lru_cache()
+        def solve(idx):
+            if idx >= n:
+                return 0
+            if idx ==n-1:
+                return cost[n-1]
+            return min(solve(idx + 1), solve(idx + 2)) + cost[idx]
+        return min(solve(0), solve(1))
