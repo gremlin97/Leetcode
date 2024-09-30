f4f71ab1c6d5644fa2375291099dff70f451fd50
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-09-29 19:17:37 -0700

    Time: 4055 ms (19.61%), Space: 113.3 MB (5.54%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -3,17 +3,18 @@ class Solution:
         max_len = 0
         res = ''
         
-        # mem = {}
         mem = []
+        
         for i in range(len(s)):
             mem.append([-1] * len(s))
-        # @lru_cache()
+            
         def recur(i,j):
             if i>=j:
                 return True
+            
             elif mem[i][j]!= -1:
                 return mem[i][j]
-                # return mem[(i,j)]
+            
             elif s[i] == s[j]:
                 mem[i][j] = recur(i+1,j-1)
                 return mem[i][j]
@@ -27,5 +28,7 @@ class Solution:
                     max_len = j-i+1
                     res = s[i:j+1]
         return res
+    
+    
                 
         
