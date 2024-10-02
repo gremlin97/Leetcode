61547001a096344c37cd3a37ff37da41cfa9843c
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-10-01 17:41:36 -0700

    Time: 40 ms (83.54%), Space: 19.6 MB (6.20%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -10,11 +10,10 @@ class Solution:
         def inorder(r):
             if not r:
                 return
+            
             inorder(r.left)
             trav.append(r.val)
             inorder(r.right)
-        
+            
         inorder(root)
-        print(trav)
         return trav[k-1]
-        
