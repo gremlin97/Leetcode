8aaec7cc64a1dee616e5cb6823b8d3f45e74358a
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-10-06 00:03:07 -0700

    Time: 43 ms (39.20%), Space: 16.7 MB (16.57%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,18 +1,13 @@
-from functools import cmp_to_key
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        
-        def cmp_fx(x,y):
-            if x+y > y+x:
-                return 1
-            elif x == y:
-                return 0
-            else:
-                return -1
+        def fx(x,y):
+            if x+y>y+x: return 1
+            elif x == y: return 0
+            else: return -1
         
         n = [str(x) for x in nums]
-        n.sort(key = cmp_to_key(cmp_fx), reverse = True)
+        n.sort(key = cmp_to_key(fx), reverse=True)
         res = ''.join(n).lstrip('0')
-        if not res: return '0'
+        if not res:
+            return '0'
         return res
-        
