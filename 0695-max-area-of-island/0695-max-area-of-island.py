c51c3ecc40dd03feb7e01c879840b163526145a1
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-10-07 16:11:13 -0700

    Time: 102 ms (79.52%), Space: 17.2 MB (51.65%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -3,33 +3,27 @@ class Solution:
         rows = len(grid)
         cols = len(grid[0])
         
-        curr_area = 0
-        
         d = [[0,1],[0,-1],[1,0],[-1,0]]
         
         res = 0
         
         def dfs(r,c):
-            nonlocal curr_area
+            curr = 0
             if 0<=r<len(grid) and 0<=c<len(grid[0]) and grid[r][c] == 1:
                 grid[r][c] = -1
                 
                 for a, b in d:
-                    if dfs(r+a, c+b):
-                        curr_area += 1
+                    curr += dfs(r+a, c+b)
                 
-                return True
+                return 1 + curr
             else:
-                return False
+                return 0
             
         
         for r in range(rows):
             for c in range(cols):
-                curr_area = 0
                 if grid[r][c] == 1:
-                    curr_area += 1
-                    dfs(r,c)
-                res = max(res, curr_area) 
+                    res = max(res,dfs(r,c))
         return res
                 
                 
