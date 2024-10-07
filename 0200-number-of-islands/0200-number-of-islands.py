6f37f4f02305938c2a7373ad43057b68c0274ee9
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-10-07 01:45:50 -0700

    Time: 246 ms (55.88%), Space: 19 MB (50.90%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         res = 0
-        def dfs(r, c):
+        
+        d = [[0,1],[0,-1],[1,0],[-1,0]]
+        
+        def dfs(r,c):
             if 0<=r<len(grid) and 0<=c<len(grid[0]) and grid[r][c] == '1':
-                
-                grid[r][c] = '-1'
-                
-                directions = [[0,1],[0,-1],[1,0],[-1,0]]
-                
-                for a, b in directions:
+                grid[r][c] = -1
+                for a, b in d:
                     dfs(r+a, c+b)
-                    
-            return
+                return
+            else:
+                return 
         
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 if grid[r][c] == '1':
                     dfs(r,c)
                     res += 1
-                
-        return res
-                    
-            
-            
         
+        return res
