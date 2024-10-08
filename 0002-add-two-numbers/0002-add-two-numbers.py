fd3ba4d9c1d71fa1720682c63ad658065b1ad3f7
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-10-07 19:11:25 -0700

    Time: 65 ms (7.13%), Space: 16.6 MB (79.84%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,34 +5,37 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        s1 = ''
-        s2 = ''
-        curr1 = l1
-        curr2 = l2
+        s1, s2 = l1, l2
         
-        while curr1:
-            s1 += str(curr1.val)
-            curr1 = curr1.next
-            
-        while curr2:
-            s2 += str(curr2.val)
-            curr2 = curr2.next
-            
-        s1 = s1[::-1]
-        s2 = s2[::-1]
-            
-        s3 = str(int(s1) + int(s2))
-        s3 = list(s3)
-        s3 = s3[::-1]
+        n1, n2 = '', ''
         
-        head = ListNode(int(s3[0]))
-        curr = head
         
-        for i in range(1, len(s3)):
-            curr.next = ListNode(int(s3[i]))
-            curr = curr.next
+        while s1:
+            n1 += str(s1.val)
+            s1 = s1.next
+        n1 = n1[::-1]
         
+        
+        while s2:
+            n2 += str(s2.val)
+            s2 = s2.next
+        n2 = n2[::-1]
+        
+        
+        add = str(int(n1) + int(n2))
+        add = add[::-1]
+        
+        res = ListNode(add[0])
+        head = res
+        for x in add[1:]:
+            res.next = ListNode(int(x))
+            res = res.next
         return head
+            
+            
+        
+            
+        
             
             
         
