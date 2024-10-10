255b33f9875dbb96d7cc5da0e587b4dbc8a4292e
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-10-10 16:19:34 -0700

    Time: 45 ms (27.01%), Space: 16.8 MB (15.22%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -3,16 +3,22 @@ class Solution:
         
         # if len(s) ==  1 and s in wordDict: return True
         
-        @lru_cache()
+        memo = {}
+    
         def build(i):
             
             if i == len(s):
                 return True
             
+            if i in memo:
+                return memo[i]
+            
             for e in range(i, len(s)):
                 if s[i:e+1] in wordDict:
                     if build(e+1):
+                        memo[i] = True
                         return True
+            memo[i] = False
         
         return build(0)
                     
